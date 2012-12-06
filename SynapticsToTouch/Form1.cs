51ad0bb7e1c3d9fb787f5e5844c6ab871ef91220
Jesse <jessenic@digiex.net>
2012-12-06 18:08:28 +0200

    Touch now works, tapping does not

--- SynapticsToTouch/Form1.cs
+++ SynapticsToTouch/Form1.cs
@@ -17,6 +17,16 @@ namespace SynapticsToTouch
         SynDeviceCtrl synDev;
         SynPacketCtrl synPacket;
         int DeviceHandle = -1;
+
+        public int XMin;
+        public int XMax;
+        public int YMin;
+        public int YMax;
+        public int ZTouchThreshold;
+
+        public int wWidth;
+        public int wHeight;
+
         public Form1()
         {
             InitializeComponent();
@@ -25,11 +35,42 @@ namespace SynapticsToTouch
             synPacket = new SynPacketCtrlClass();
         }
 
+        PointerTouchInfo[] contacts = new PointerTouchInfo[1];
         void synDev_OnPacket()
         {
+            int X, Y;
             synDev.LoadPacket(synPacket);
-            if(synPacket.X != 0 || synPacket.Y != 0)
-            Debug.WriteLine(synPacket.X + ", " + synPacket.Y + ", " + synPacket.FingerState);
+            X = (synPacket.X - XMin) * wWidth / (XMax - XMin);
+            Y = (YMax - synPacket.Y) * wHeight / (YMax - YMin);
+            //Finger = synPacket.FingerState & (int)SynFingerFlags.SF_FingerPresent;
+            if (contacts[0].PointerInfo.PointerId == 0 && synPacket.FingerState != 0)
+            {
+                contacts[0] = MakePointerTouchInfo(X, Y, 2, 1);
+                //contacts[1] = MakePointerTouchInfo(650, 500, 2, 2);
+                bool success = TouchInjector.InjectTouchInput(1, contacts);
+            }
+            else if (synPacket.FingerState != 0)
+            {
+                contacts[0].PointerInfo.PtPixelLocation.X = X;
+                contacts[0].PointerInfo.PtPixelLocation.Y = Y;
+                contacts[0].PointerInfo.PointerFlags = PointerFlags.UPDATE | PointerFlags.INRANGE | PointerFlags.INCONTACT;
+                //contacts[1].PointerInfo.PointerFlags = PointerFlags.UPDATE | PointerFlags.INRANGE | PointerFlags.INCONTACT;
+
+                bool s = TouchInjector.InjectTouchInput(1, contacts);
+            }
+            else
+            {
+                //release them
+                contacts[0].PointerInfo.PointerFlags = PointerFlags.UP;
+                //contacts[1].PointerInfo.PointerFlags = PointerFlags.UP;
+
+                bool success2 = TouchInjector.InjectTouchInput(1, contacts);
+                contacts[0].PointerInfo.PointerId = 0;
+            }
+        }
+        public Rectangle GetScreen()
+        {
+            return Screen.FromControl(this).Bounds;
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -42,14 +83,49 @@ namespace SynapticsToTouch
                 MessageBox.Show("Unable to find a Synaptics TouchPad");
 
             }
-            synDev.Select(DeviceHandle);
-            synDev.Activate();
-            synDev.OnPacket += synDev_OnPacket;
+            else
+            {
+                synDev.Select(DeviceHandle);
+                synDev.Activate();
+                bool s = TouchInjector.InitializeTouchInjection();//initialize with default settings
+
+                XMin = synDev.GetLongProperty(SynDeviceProperty.SP_XLoSensor);
+                XMax = synDev.GetLongProperty(SynDeviceProperty.SP_XHiSensor);
+                YMin = synDev.GetLongProperty(SynDeviceProperty.SP_YLoSensor);
+                YMax = synDev.GetLongProperty(SynDeviceProperty.SP_YHiSensor);
+                ZTouchThreshold = synDev.GetLongProperty(SynDeviceProperty.SP_ZTouchThreshold) + 20;
+                wHeight = GetScreen().Height;
+                wWidth = GetScreen().Width; 
+                synDev.Acquire(0);
+
+                synDev.OnPacket += synDev_OnPacket;
+            }
         }
 
+        private PointerTouchInfo MakePointerTouchInfo(int x, int y, int radius, uint id, uint orientation = 90, uint pressure = 32000)
+        {
+            PointerTouchInfo contact = new PointerTouchInfo();
+            contact.PointerInfo.pointerType = PointerInputType.TOUCH;
+            contact.TouchFlags = TouchFlags.NONE;
+            contact.Orientation = orientation;
+            contact.Pressure = pressure;
+            contact.PointerInfo.PointerFlags = PointerFlags.DOWN | PointerFlags.INRANGE | PointerFlags.INCONTACT;
+            contact.TouchMasks = TouchMask.CONTACTAREA | TouchMask.ORIENTATION | TouchMask.PRESSURE;
+            contact.PointerInfo.PtPixelLocation.X = x;
+            contact.PointerInfo.PtPixelLocation.Y = y;
+            contact.PointerInfo.PointerId = id;
+            contact.ContactArea.left = x - radius;
+            contact.ContactArea.right = x + radius;
+            contact.ContactArea.top = y - radius;
+            contact.ContactArea.bottom = y + radius;
+            return contact;
+        }
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
-            synDev.Unacquire();
+            if (DeviceHandle != -1)
+            {
+                synDev.Unacquire();
+            }
         }
     }
 }
